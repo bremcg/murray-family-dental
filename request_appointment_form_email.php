bb74b43786e5148f974f28fd2b9c0e78b0e89354
andrewariley87 <andrewariley87@gmail.com>
2019-05-12 16:26:44 -0400

    Adds Email to Appoinment Form
    The request appointment form now uses a php script to send an email
    (right now it is my email for testing purposes).  The script runs
    when running the app on a php server.  The only problem is the email
    does not send.  This might be something to test once the site is live.
    
    You can test by starting a server `php -S localhost:8000`

---
+++ request_appointment_form_email.php
@@ -0,0 +1,96 @@
+<?php
+if(isset($_POST['email'])) {
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "andrewariley87@gmail.com";
+    $email_subject = "Schedule Appointment for {$_POST['first_name']} {$_POST['last_name']}";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+    // validation expected data exists
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['last_name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['phone_number']) ||
+        !isset($_POST['appointment_date']) ||
+        !isset($_POST['preffered_time']) ||
+        !isset($_POST['symptoms'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+     
+ 
+    $first_name = $_POST['first_name']; // required
+    $last_name = $_POST['last_name']; // required
+    $email_from = $_POST['email']; // required
+    $phone_number = $_POST['phone_number']; // not required
+    $appointment_date = $_POST['appointment_date']; // already formatted by form
+    $preffered_time = $_POST['preffered_time']; // already formatted by form
+    $symptoms = $_POST['symptoms']; // required
+ 
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(strlen($symptoms) < 2) {
+    $error_message .= 'The symptoms you entered do not appear to be valid.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+     
+ 
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Phone Number: ".clean_string($phone_number)."\n";
+    $email_message .= "Date: ".clean_string($appointment_date)."\n";
+    $email_message .= "Time: ".clean_string($preffered_time)."\n";
+    $email_message .= "Symptoms: ".clean_string($symptoms)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+ 
+<!-- include your own success html here -->
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+<?php
+ 
+}
+?>
